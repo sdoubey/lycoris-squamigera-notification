6480674a4fc931a1a4541f139b9a950365dd36b3
Sébastien Doubey <sebastien.doubey@gmail.com>
2019-09-24 23:42:22 +0200

    Add LycorisSubjectMessage impl

--- src/main/java/eu/lycoris/squamigera/notification/common/Notification.java
+++ src/main/java/eu/lycoris/squamigera/notification/common/Notification.java
@@ -2,6 +2,7 @@ package eu.lycoris.squamigera.notification.common;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 
+import eu.lycoris.spring.common.LycorisSubjectMessage;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
@@ -9,7 +10,7 @@ import lombok.ToString;
 @Getter
 @ToString
 @AllArgsConstructor(onConstructor_ = @JsonCreator)
-public class Notification {
+public class Notification implements LycorisSubjectMessage{
 
   private String subject;
 }
